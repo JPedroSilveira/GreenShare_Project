a291f468ce0f430b717da3ff0194122ac072d13f
JPedroSSilva <joao.silva@cwi.com.br>
2017-09-27 00:20:58 -0300

    feat(Controller): implementação de controller abstrata com métodos reutilizáveis

---
+++ SeedShareWS/src/main/java/com/seedshare/controller/Controller.java
@@ -0,0 +1,25 @@
+package com.seedshare.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.seedshare.entity.User;
+import com.seedshare.service.user.UserServiceImpl;
+
+public abstract class Controller {
+	
+	@Autowired
+	UserServiceImpl userService;
+	
+	protected User getCurrentUser() {
+    	Object currentUser = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    	String email;
+    	if (currentUser instanceof UserDetails ) {
+    		email= ( (UserDetails)currentUser).getUsername();
+    	} else {
+    		email = currentUser.toString();
+    	}
+    	return userService.findOneByEmail(email);
+    }
+}
