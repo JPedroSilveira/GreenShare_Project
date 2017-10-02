8a537a0e50922a594f516b796ee64eee72c23d6b
JPedroSSilva <joao.silva@cwi.com.br>
2017-10-02 00:19:43 -0300

    feat(OfferController): implementação de controller para Offer

---
+++ SeedShareWS/src/main/java/com/seedshare/controller/offer/OfferController.java
@@ -0,0 +1,29 @@
+package com.seedshare.controller.offer;
+
+import org.springframework.http.ResponseEntity;
+
+import com.seedshare.controller.BasicController;
+import com.seedshare.entity.FlowerShop;
+import com.seedshare.entity.Offer;
+import com.seedshare.entity.Species;
+import com.seedshare.entity.User;
+
+/**
+ * @author joao.silva
+ */
+public interface OfferController extends BasicController<Offer, Long>{
+	
+	ResponseEntity<?> delete(Long id);
+
+	ResponseEntity<?> findAllByUser(Long id);
+
+	ResponseEntity<?> findAllByUser(User user);
+
+	ResponseEntity<?> findAllByFlowerShop(FlowerShop flowerShop);
+
+	ResponseEntity<?> findAllByFlowerShop(Long id);
+
+	ResponseEntity<?> findAllBySpecies(Species species);
+
+	ResponseEntity<?> findAllBySpecies(Long id);
+}
---
+++ SeedShareWS/src/main/java/com/seedshare/controller/offer/OfferControllerImpl.java
@@ -0,0 +1,118 @@
+package com.seedshare.controller.offer;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.seedshare.entity.FlowerShop;
+import com.seedshare.entity.Offer;
+import com.seedshare.entity.Species;
+import com.seedshare.entity.User;
+import com.seedshare.service.offer.OfferServiceImpl;
+
+/**
+ * @author joao.silva
+ */
+@RestController
+@RequestMapping("/offer")
+public class OfferControllerImpl implements OfferController{
+
+	@Autowired
+	OfferServiceImpl offerService;
+	
+	@Override
+	@PostMapping("/save/")
+	public ResponseEntity<?> save(Offer offer) {
+		Offer response = offerService.save(offer);
+		if(response != null) {
+			response.setUser(null);
+			return new ResponseEntity<Offer>(response, HttpStatus.OK);   
+		}
+		return new ResponseEntity<String>("Falha ao registrar oferta", HttpStatus.NOT_ACCEPTABLE);
+	}
+
+	@Override
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		offerService.delete(id);
+		return new ResponseEntity<String>("Oferta retirada.", HttpStatus.OK);
+	}
+	
+	@Override
+	@DeleteMapping("/delete/")
+	public ResponseEntity<?> delete(@RequestBody Offer offer) {
+		offerService.delete(offer);
+		return new ResponseEntity<String>("Oferta retirada.", HttpStatus.OK);
+	}
+
+	@Override
+	@GetMapping("/get/{id}")
+	public ResponseEntity<?> findOne(@PathVariable Long id) {
+		Offer response = offerService.findOne(id);
+		if(response != null) {
+			response.setUser(null);
+			return new ResponseEntity<Offer>(response, HttpStatus.OK);   
+		}
+		return new ResponseEntity<String>("Falha ao registrar oferta", HttpStatus.NOT_ACCEPTABLE);
+	}
+	
+	@Override
+	@GetMapping("/getByUser/")
+	public ResponseEntity<?> findAllByUser(@RequestBody User user) {
+		List<Offer> response = offerService.findAllByUser(user);
+		return findAllResponse(response);
+	}
+
+	@Override
+	@GetMapping("/getByUser/{id}")
+	public ResponseEntity<?> findAllByUser(@PathVariable Long id) {
+		List<Offer> response = offerService.findAllByUser(id);
+		return findAllResponse(response);
+	}
+	
+	@Override
+	@GetMapping("/getByFlowerShop/")
+	public ResponseEntity<?> findAllByFlowerShop(@RequestBody FlowerShop flowerShop) {
+		List<Offer> response = offerService.findAllByFlowerShop(flowerShop);
+		return findAllResponse(response);
+	}
+	
+	@Override
+	@GetMapping("/getByFlowerShop/{id}")
+	public ResponseEntity<?> findAllByFlowerShop(@PathVariable Long id) {
+		List<Offer> response = offerService.findAllByFlowerShop(id);
+		return findAllResponse(response);
+	}
+	
+	@Override
+	@GetMapping("/getBySpecies/")
+	public ResponseEntity<?> findAllBySpecies(@RequestBody Species species) {
+		List<Offer> response = offerService.findAllBySpecies(species);
+		return findAllResponse(response);
+	}
+	
+	@Override
+	@GetMapping("/getBySpecies/{id}")
+	public ResponseEntity<?> findAllBySpecies(@PathVariable Long id) {
+		List<Offer> response = offerService.findAllBySpecies(id);
+		return findAllResponse(response);
+	}
+	
+	private ResponseEntity<?> findAllResponse(List<Offer> response) {
+		if(response != null) {
+			response.forEach(r -> r.setUser(null));
+			return new ResponseEntity<List<Offer>>(response, HttpStatus.OK);   
+		}
+		return new ResponseEntity<String>("Falha ao carregar ofertas", HttpStatus.NOT_FOUND);
+	}
+
+}
