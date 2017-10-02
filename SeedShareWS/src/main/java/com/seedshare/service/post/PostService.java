1d05e0026730644434b7613714a9007bfba5bb6c
JPedroSSilva <joao.silva@cwi.com.br>
2017-10-02 00:25:50 -0300

    feat(PostService): implementação de Service para Post

---
+++ SeedShareWS/src/main/java/com/seedshare/service/post/PostService.java
@@ -0,0 +1,12 @@
+package com.seedshare.service.post;
+
+import com.seedshare.entity.Post;
+import com.seedshare.service.BasicService;
+
+/**
+ * Service interface of Post
+ * @author joao.silva
+ */
+public interface PostService extends BasicService<Post,Long>{
+	
+}
---
+++ SeedShareWS/src/main/java/com/seedshare/service/post/PostServiceImpl.java
@@ -0,0 +1,37 @@
+package com.seedshare.service.post;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.seedshare.entity.Post;
+import com.seedshare.repository.PostRepository;
+
+/**
+ * Implementation of Post Service interface
+ * @author joao.silva
+ */
+@Service
+public class PostServiceImpl implements PostService{
+
+	@Autowired
+    PostRepository postRepository;
+	
+	@Override
+	public Post save(Post post) {
+		if(post.generateNewValidation().isValid()) {
+			return postRepository.save(post);
+		}
+		return null;
+	}
+
+	@Override
+	public void delete(Post post) {
+		postRepository.delete(post);
+	}
+
+	@Override
+	public Post findOne(Long id) {
+		return postRepository.findOne(id);
+	}
+
+}
