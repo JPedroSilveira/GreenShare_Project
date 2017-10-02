c77fd095369e723d438a482668fc9017da10425f
JPedroSSilva <joao.silva@cwi.com.br>
2017-10-02 00:24:12 -0300

    fix(identamento)

--- SeedShareWS/src/main/java/com/seedshare/repository/UserRepository.java
+++ SeedShareWS/src/main/java/com/seedshare/repository/UserRepository.java
@@ -4,12 +4,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.seedshare.entity.User;
+
 /**
  * Repository Class for User
  * @author joao.silva
  */
 @Repository
-public interface  UserRepository extends CrudRepository<User, Long>{
+public interface UserRepository extends CrudRepository<User, Long>{
 	User findOneByEmail(String email);
 	User findOneByCpf(String cpf);
 }
