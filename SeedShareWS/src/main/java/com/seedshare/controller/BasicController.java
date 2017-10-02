5c50841ba72d28f579080feadb668b78e62fe5b1
JPedroSSilva <joao.silva@cwi.com.br>
2017-10-02 00:17:34 -0300

    fix(BasicController): mudança de retorno para ResponseEntity<?>

--- SeedShareWS/src/main/java/com/seedshare/controller/BasicController.java
+++ SeedShareWS/src/main/java/com/seedshare/controller/BasicController.java
@@ -1,14 +1,17 @@
 package com.seedshare.controller;
 
+import org.springframework.http.ResponseEntity;
+
 /**
  * @author joao.silva
  * @param <Entity>
  * @param <ID>
  */
 public interface BasicController<Entity,ID> {
-    Entity save(Entity e);
+	
+	ResponseEntity<?> save(Entity e);
     
-    void delete(Entity e);
+	ResponseEntity<?> delete(Entity e);
 
-    Entity findOne(ID id);
+	ResponseEntity<?> findOne(ID id);
 }
