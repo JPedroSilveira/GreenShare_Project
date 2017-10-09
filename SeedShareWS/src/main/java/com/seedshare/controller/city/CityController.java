4a5c076736fb3307974aaedb5b60538e8bdd1339
JPedroSSilva <joao.silva@cwi.com.br>
2017-10-09 01:39:56 -0300

    feat(CityController)

---
+++ SeedShareWS/src/main/java/com/seedshare/controller/city/CityController.java
@@ -0,0 +1,18 @@
+package com.seedshare.controller.city;
+
+import org.springframework.http.ResponseEntity;
+
+import com.seedshare.controller.BasicController;
+import com.seedshare.entity.City;
+
+/**
+ * Controller Interface for City
+ * 
+ * @author joao.silva
+ */
+public interface CityController extends BasicController<City, Long> {
+
+	ResponseEntity<?> findByCountry(Long id);
+
+	ResponseEntity<?> findByState(Long id);
+}
