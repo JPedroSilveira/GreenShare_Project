d477ec7bbcc9ba025824f3d922c95e34208f7adb
JPedroSSilva <joao.silva@cwi.com.br>
2017-10-09 01:40:39 -0300

    feat(ClimateController)

---
+++ SeedShareWS/src/main/java/com/seedshare/controller/climate/ClimateController.java
@@ -0,0 +1,15 @@
+package com.seedshare.controller.climate;
+
+import org.springframework.http.ResponseEntity;
+
+import com.seedshare.controller.BasicController;
+import com.seedshare.entity.Climate;
+
+/**
+ * Controller Interface for City
+ * 
+ * @author joao.silva
+ */
+public interface ClimateController extends BasicController<Climate, Long> {
+	ResponseEntity<?> findAll();
+}
---
+++ SeedShareWS/src/main/java/com/seedshare/controller/climate/ClimateControllerImpl.java
@@ -0,0 +1,55 @@
+package com.seedshare.controller.climate;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.seedshare.entity.Climate;
+import com.seedshare.helpers.IsHelper;
+import com.seedshare.service.climate.ClimateService;
+
+/**
+ * Controller class for Climate
+ * 
+ * @author joao.silva
+ */
+@RestController
+@RequestMapping("/climate")
+public class ClimateControllerImpl extends IsHelper implements ClimateController {
+
+	@Autowired
+	ClimateService climateService;
+
+	@Override
+	@PostMapping("/")
+	public ResponseEntity<?> save(@RequestBody @Valid Climate climate) {
+		return climateService.save(climate);
+	}
+
+	@Override
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		return climateService.delete(id);
+	}
+
+	@Override
+	@GetMapping("/{id}")
+	public ResponseEntity<?> findOne(@PathVariable Long id) {
+		return climateService.findOne(id);
+	}
+
+	@Override
+	@GetMapping("/")
+	public ResponseEntity<?> findAll() {
+		return climateService.findAll();
+	}
+
+}
