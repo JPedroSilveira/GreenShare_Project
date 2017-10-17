6cb51f0938bc779453510e37a5e0d3a80967b5aa
JPedroSSilva <joao.silva@cwi.com.br>
2017-10-17 10:52:10 -0200

    feat(PermissionService)

---
+++ SeedShareWS/src/main/java/com/seedshare/service/permission/PermissionService.java
@@ -0,0 +1,18 @@
+package com.seedshare.service.permission;
+
+import org.springframework.http.ResponseEntity;
+
+import com.seedshare.entity.Permission;
+import com.seedshare.service.BasicService;
+
+/**
+ * Service interface of Permission
+ * 
+ * @author joao.silva
+ * @author gabriel.schneider
+ */
+public interface PermissionService extends BasicService<Permission, Long> {
+
+	ResponseEntity<?> findAll();
+
+}
---
+++ SeedShareWS/src/main/java/com/seedshare/service/permission/PermissionServiceImpl.java
@@ -0,0 +1,33 @@
+package com.seedshare.service.permission;
+
+import org.springframework.http.ResponseEntity;
+
+import com.seedshare.entity.Permission;
+
+public class PermissionServiceImpl implements PermissionService{
+
+	@Override
+	public ResponseEntity<?> save(Permission permission) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<?> delete(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<?> findOne(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<?> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
